a0c205a66a8ae770537bc075f7a3142e06bae8d7
Paulo Santana <paulosantana.sa@gmail.com>
2023-11-20 02:47:06 -0300

    feat: Add `Calendar`, `ScheduleForm` and `CalendarStep` components

---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,11 @@
+import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
+
+export function ScheduleForm() {
+  return (
+    <>
+      <CalendarStep />
+      {/* <ConfirmStep /> */}
+    </>
+  )
+}
